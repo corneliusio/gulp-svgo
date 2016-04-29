feb8bab807efe9cc020fcb68eb1954501f5b9837
corneliusio <guy@cornelius.io>
2016-04-29 11:18:18 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gulp-svgo
+Optimizing SVG vector graphics files with Gulp
---
+++ index.js
@@ -0,0 +1,38 @@
+var fs = require('fs'),
+    path = require('path'),
+    Stream = require('stream'),
+    SVGO = require('svgo');
+
+module.exports = function(options) {
+
+    var stream = new Stream.Transform({ objectMode: true }),
+        settings = Object.assign({}, options),
+        svgo = new SVGO(settings);
+
+    stream._transform = function(file, encoding, next) {
+        var flow = this;
+
+        if (!file.checksum) {
+
+            if (path.extname(file.path).toLowerCase() !== '.svg') {
+                flow.push(file);
+                return next();
+            }
+
+            if (file.isStream()) {
+                flow.push(file);
+                return next();
+            }
+
+            if (file.isBuffer()) {
+                svgo.optimize(file.contents.toString('utf8'), function(result) {
+                    file.contents = new Buffer(result.data);
+                    flow.push(file);
+                    return next();
+                });
+            }
+        }
+    };
+
+    return stream;
+};
